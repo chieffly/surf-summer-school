67719059bcd314977e21e8048c585c4b36cb32a6
Anton Bryantsev <chieffly@ya.ru>
2019-08-21 22:53:12 +0300

    Начальный коммит

---
+++ app/src/main/java/ru/chieffly/memoscope/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.chieffly.memoscope
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println("Hello ANton");
+    }
+}
