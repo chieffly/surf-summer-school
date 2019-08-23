fca65787787a47e9f4a901a5ef07963d85e4a4a5
Anton Bryantsev <chieffly@ya.ru>
2019-08-23 20:33:47 +0300

    Инициализация и верстка главного экрана Создан Toolbar FrameLayout и BottomNavigationView

--- app/src/main/java/ru/chieffly/memoscope/MainActivity.kt
+++ app/src/main/java/ru/chieffly/memoscope/MainActivity.kt
@@ -2,14 +2,51 @@ package ru.chieffly.memoscope
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+
+
+class MainActivity : AppCompatActivity(),  BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //loading the default fragment
+        loadFragment(FragmentDash())
+
+        //getting bottom navigation view and attaching the listener
+        val navigation = findViewById<BottomNavigationView>(R.id.navigation)
+        navigation.setOnNavigationItemSelectedListener(this)
+    }
 
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        var fragment: Fragment? = null
+
+        when (item.getItemId()) {
+
+            R.id.navigation_dashboard -> fragment = FragmentDash()
+
+            R.id.navigation_person -> fragment = FragmentPerson()
+
+            R.id.navigation_add -> fragment = FragmentAdd()
+        }
+
+        return loadFragment(fragment)
+    }
+
+    private fun loadFragment(fragment: Fragment?): Boolean {
+        //switching fragment
+        if (fragment != null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment!!)
+                .commit()
+            return true
+        }
+        return false
     }
 }
